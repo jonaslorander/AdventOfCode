fc07f1d7abde1f2137a2fc0781eac20f7637e54f
Jonas Lorander <jonas@lorander.com>
2024-12-01 15:03:46 +0100

    Solution for day 1, 2024

--- 2024/01/solution2.py
+++ 2024/01/solution2.py
@@ -7,13 +7,12 @@ list_right = []
 for row in rows:
     distances = row.split("   ")
     list_left.append(int(distances[0]))
+
     list_right.append(int(distances[1]))
 
-list_left.sort()
-list_right.sort()
-
 for i in range(len(list_left)):
-    total_differance = total_differance + abs(list_left[i] - list_right[i])
+    occurances = list_right.count(list_left[i])
+    total_differance = total_differance + (list_left[i] * occurances)
 
 
 print(total_differance)
