914f70b293cb84148e50496b364b35f960ef0107
Jonas Lorander <jonas@lorander.com>
2024-12-02 20:33:29 +0100

    Solution for day 2, 2024
    Damn that's an ugly solution, but it works.

--- 2024/02/solution2.py
+++ 2024/02/solution2.py
@@ -2,15 +2,13 @@ rows = open("input.txt").read().splitlines()
 
 total_safe = 0
 
-def check_safety(row):
+def recheck_safety(reports):
     last_increasing = None
     this_increasing = None
-    reports = row.split(" ")
-    #print(reports)
+    print("B", reports)
     for i in range(len(reports) - 1):
         diff = int(reports[i]) - int(reports[i + 1])
 
-        #print(diff)
         if diff > 0:
             this_increasing = False
         elif diff < 0:
@@ -19,7 +17,6 @@ def check_safety(row):
         if abs(diff) not in range(1, 4):
             return False
 
-        #print(this_increasing)
         if (this_increasing != last_increasing) and last_increasing != None:
             return False
         
@@ -27,10 +24,55 @@ def check_safety(row):
     
     return True
 
+def check_safety(row):
+    last_increasing = None
+    this_increasing = None
+    reports0 = row.split(" ")
+    print("A", reports0)
+    for i in range(len(reports0) - 1):
+        diff = int(reports0[i]) - int(reports0[i + 1])
+
+        #print(diff)
+        if diff > 0:
+            this_increasing = False
+        elif diff < 0:
+            this_increasing = True
+
+        if diff == 0:
+            reports1 = list(reports0)
+            reports2 = list(reports0)
+            reports0.pop(i)
+            reports1.pop(i + 1)
+            reports2.pop(i - 1)
+            return recheck_safety(reports0) or recheck_safety(reports1) or recheck_safety(reports2)
+
+        if abs(diff) not in range(1, 4):
+            reports1 = list(reports0)
+            reports2 = list(reports0)
+            reports0.pop(i)
+            reports1.pop(i + 1)
+            reports2.pop(i - 1)
+            return recheck_safety(reports0) or recheck_safety(reports1) or recheck_safety(reports2)
+
+        #print(this_increasing)
+        if (this_increasing != last_increasing) and last_increasing != None:
+            reports1 = list(reports0)
+            reports2 = list(reports0)
+            reports0.pop(i)
+            reports1.pop(i + 1)
+            reports2.pop(i - 1)
+            return recheck_safety(reports0) or recheck_safety(reports1) or recheck_safety(reports2)
+        
+        last_increasing = this_increasing
+    
+    return True
+
 for row in rows:
     is_safe = check_safety(row)
-    #print(is_safe)
     if is_safe:
         total_safe = total_safe + 1
+    else:
+        print(False)
+        pass
 
 print(total_safe)
