50541baf29f46a334126b2c94f3dd21720d75984
Jonas Lorander <jonas@lorander.com>
2025-12-14 22:51:07 +0100

    Solution for part 2 day 2 complete

--- 2025/02/solution2.py
+++ 2025/02/solution2.py
@@ -1,13 +1,27 @@
-ids = open("sampleinput.txt").read().replace('\r', '').replace('\n', '').split(',')
+ids = open("input.txt").read().replace('\r', '').replace('\n', '').split(',')
+
+def get_chunks(lst, n):
+    ret_list = []
+    for i in range(0, len(lst), n):
+        ret_list.append(lst[i:i + n])
+
+    return ret_list
 
 def find_invalid_ids(start, stop):
     id_list = list(range(int(start), int(stop) + 1))
     id_list = [str(id) for id in id_list]
-
     sum = 0
+
     for id in id_list:
-        if id[0:int(len(id)/2)] == id[int(len(id)/2):len(id)]:
-            sum = sum + int(id)
+        i = 1
+        while i <= len(id) / 2:
+            chunks = get_chunks(id, i)
+            equal = all(c == chunks[0] for c in chunks)
+            i += 1
+
+            if equal:
+                sum += int(id)
+                break
 
     return sum
 
